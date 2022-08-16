7df57ed9665de3d617efe5bc6281dfa14c3ed403
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-08-16 09:38:14 +0800

    Added identification number field

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -31,6 +31,9 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'is_admin' => fake()->boolean(),
+            'identification_number' => fake()->randomNumber(6, true) . '-' 
+                . str_pad(fake()->randomElement( range(1, 14) ), 2, "0", STR_PAD_LEFT) . '-' 
+                . fake()->randomNumber(4, true),
         ];
     }
 
